ccf8a97a54a09d5319aecf433a1400738cbf0afe
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-09-23 11:48:44 +0300

    bugfix spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,17 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] GameObject zombiePrefab;
     [SerializeField] float spawnRate = 5f;
+    [SerializeField] float spawnDistance = 5f;
+    Player player;
     private void Start()
     {
-        InvokeRepeating("SpawnZombie", 5f, spawnRate);
+        player = FindFirstObjectByType<Player>();
+        InvokeRepeating(nameof(SpawnZombie), 5f, spawnRate);
     }
     private void SpawnZombie()
     {
-        Instantiate(zombiePrefab, new Vector3(Random.Range(-8f, 8f), 0.5f, 0f), Quaternion.identity);
+        float direction = Random.value > 0.5f ? 1f : -1f;
+        float positionX = player.transform.position.x + (direction * spawnDistance);
+        Instantiate(zombiePrefab, new Vector3(positionX, 0.5f, 0f), Quaternion.identity);
     }
 }
